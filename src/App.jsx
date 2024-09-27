1120abd9de44f6cc226ec820f8bcdd981bbf9b82
Adornadowilliam2 <adornadowilliam@gmail.com>
2024-09-27 13:10:34 +0800

    make it database

--- src/App.jsx
+++ src/App.jsx
@@ -3,63 +3,62 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import bg from "./assets/bg.png";
-
+import pitbull from "./assets/pitbull.png";
+import husky from "./assets/husky.png";
+import saintbernard from "./assets/saintbernard.png";
+import germanshepherd from "./assets/germanshepherd.png";
+import shibainu from "./assets/shibainu.png";
+import chihuahua from "./assets/chihuahua.png";
+import bermesemountain from "./assets/bermesemountain.png";
 const data = [
   {
     id: 1,
     name: "Iron Man",
     breed: "Husky",
     age: "5",
-    image:
-      "https://images.unsplash.com/photo-1600486913747-6c69d0d63c69?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+    image: husky,
   },
   {
     id: 2,
     name: "Captain America",
-    breed: "Husky",
+    breed: "German Shepherd",
     age: "5",
-    image:
-      "https://images.unsplash.com/photo-1600486913747-6c69d0d63c69?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+    image: germanshepherd,
   },
   {
     id: 3,
     name: "Thor",
-    breed: "Husky",
+    breed: "Saint Bernard",
     age: "5",
-    image:
-      "https://images.unsplash.com/photo-1600486913747-6c69d0d63c69?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+    image: saintbernard,
   },
   {
     id: 4,
     name: "Hulk",
-    breed: "Husky",
+    breed: "Pitbull",
     age: "5",
-    image:
-      "https://images.unsplash.com/photo-1600486913747-6c69d0d63c69?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+    image: pitbull,
   },
   {
     id: 5,
     name: "Black Panther",
-    breed: "Husky",
+    breed: "Bermese Mountain Dog",
     age: "5",
-    image:
-      "https://images.unsplash.com/photo-1600486913747-6c69d0d63c69?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+    image: bermesemountain,
   },
   {
     id: 6,
     name: "Captain Marvel",
     breed: "Husky",
     age: "5",
-    image:
-      "https://images.unsplash.com/photo-1600486913747-6c69d0d63c69?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+    image: chihuahua,
   },
   {
     id: 7,
     name: "Spiderman",
-    breed: "Husky",
+    breed: "Shiba Inu",
     age: "5",
-    image:
-      "https://images.unsplash.com/photo-1600486913747-6c69d0d63c69?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+    image: shibainu,
   },
 ];
 function App() {
@@ -69,10 +68,12 @@ function App() {
       <h1>Dogs Data</h1>
       {data.map((dog) => (
         <div key={dog.id}>
-          <h1>{dog.name}</h1>
+          <h1>
+            {dog.id}.{dog.name}
+          </h1>
           <img src={dog.image} alt={dog.name} />
-          <p>{dog.breed}</p>
-          <p>{dog.age}</p>
+          <p>Breed: {dog.breed}</p>
+          <p>Age: {dog.age}</p>
         </div>
       ))}
     </>
